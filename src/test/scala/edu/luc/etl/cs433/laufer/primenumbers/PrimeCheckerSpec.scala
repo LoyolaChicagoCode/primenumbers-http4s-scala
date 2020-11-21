8b35b38ef2feaffda3b428edcae3ed2b9871a039
Konstantin Läufer <klaeufer@gmail.com>
2020-11-20 20:15:23 -0600

    made specs2 test table-driven
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/test/scala/edu/luc/etl/cs433/laufer/primenumbers/PrimeCheckerSpec.scala
+++ src/test/scala/edu/luc/etl/cs433/laufer/primenumbers/PrimeCheckerSpec.scala
@@ -3,33 +3,54 @@ package edu.luc.etl.cs433.laufer.primenumbers
 import cats.effect.IO
 import org.http4s._
 import org.http4s.implicits._
-import org.specs2.matcher.MatchResult
+import org.specs2.mutable.Specification
+import org.specs2.matcher.{DataTables, MatchResult}
 
-class PrimeCheckerSpec extends org.specs2.mutable.Specification {
+class PrimeCheckerSpec extends Specification with DataTables {
 
-  "PrimeChecker method" >> {
-    "6007 is prime" >> {
-      PrimeChecker.isPrime(6007) must beTrue
-    }
-    "6008 is not prime" >> {
-      PrimeChecker.isPrime(6008) must beFalse
-    }
-    "6033 is not prime" >> {
-      PrimeChecker.isPrime(6033) must beFalse
+  "PrimeChecker method works for values in table" >> {
+    primeTable |> {
+      (number, result) => PrimeChecker.isPrime(BigInt(number)) must_== result
     }
   }
 
-  "PrimeChecker service" >> {
-    "6007 is prime" >> {
-      uriReturns200(6007)
-    }
-    "6008 is not prime" >> {
-      uriReturns404(6008)
-    }
-    "6033 is not prime" >> {
-      uriReturns404(6033)
+  "PrimeChecker service works for values in table" >> {
+    primeTable |> {
+      (number, result) =>
+        if (result) uriReturns200(number) else uriReturns404(number)
     }
   }
+
+  private[this] val primeTable =
+    "number" | "result" |
+      -7 ! false |
+      -1 ! false |
+      0 ! false |
+      1 ! false |
+      2 ! true |
+      3 ! true |
+      4 ! false |
+      5 ! true |
+      6 ! false |
+      7 ! true |
+      8 ! false |
+      9 ! false |
+      10 ! false |
+      1013 ! true |
+      1014 ! false |
+      6006 ! false |
+      6007 ! true |
+      6033 ! false |
+      10007 ! true |
+      10077 ! false |
+      100003 ! true |
+      100033 ! false |
+      1000003 ! true |
+      1000013 ! false |
+      10000169 ! true |
+      10001169 ! false |
+      100000007 ! true |
+      100000077 ! false
 
   private[this] def retPrimeChecker(i: Int): Response[IO] = {
     val getHW = Request[IO](Method.GET, uri"/" / s"$i")
