72b88ac12f05b5cf43920deeb97e8509dc99d6d2
Konstantin Läufer <klaeufer@gmail.com>
2020-11-21 12:30:16 -0600

    added meaningful default page, fixes #2
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- build.sbt
+++ build.sbt
@@ -14,6 +14,7 @@ lazy val root = (project in file("."))
       "org.http4s"      %% "http4s-blaze-client" % Http4sVersion,
       "org.http4s"      %% "http4s-circe"        % Http4sVersion,
       "org.http4s"      %% "http4s-dsl"          % Http4sVersion,
+      "org.http4s"      %% "http4s-scala-xml"    % Http4sVersion,
       "io.circe"        %% "circe-generic"       % CirceVersion,
       "org.specs2"      %% "specs2-core"         % Specs2Version % Test,
       "ch.qos.logback"  %  "logback-classic"     % LogbackVersion
--- src/main/scala/edu/luc/etl/cs433/laufer/primenumbers/PrimeCheckerRoutes.scala
+++ src/main/scala/edu/luc/etl/cs433/laufer/primenumbers/PrimeCheckerRoutes.scala
@@ -2,8 +2,10 @@ package edu.luc.etl.cs433.laufer.primenumbers
 
 import cats.effect.Sync
 import cats.implicits._
-import org.http4s.{HttpRoutes, Response}
+import org.http4s.{HttpRoutes, MediaType, Response}
+import org.http4s.headers.`Content-Type`
 import org.http4s.dsl.Http4sDsl
+import org.http4s.scalaxml._
 
 object PrimeCheckerRoutes {
 
@@ -11,10 +13,22 @@ object PrimeCheckerRoutes {
     val dsl = new Http4sDsl[F]{}
     import dsl._
     HttpRoutes.of[F] {
-      case GET -> Root / number =>
-        P.check(BigInt(number)).map {
-          case true => Response(status = Ok).withEntity(number)
-          case false => Response(status = NotFound)
+      case GET -> Root =>
+        val title = "Welcome to the prime checker web service!"
+        Ok(<html>
+            <head><title>{title}</title></head>
+            <body>
+              <h1>{title}</h1>
+              <p>Append a number to the request to check whether it is a prime.</p>
+              <p>No more than ${Long.MaxValue}, please.</p>
+              <p>The code for this service is <a href="https://github.com/LoyolaChicagoCode/primenumbers-http4s-scala">on GitHub</a>.</p>
+            </body>
+          </html>
+        ).map(_.withContentType(`Content-Type`(MediaType.text.html)))
+      case GET -> Root / LongVar(number) =>
+        P.check(BigInt(number)).flatMap {
+          case true => Ok(f"Yay, $number happens to be a prime!")
+          case false => NotFound(f"Bummer, $number is not a prime.")
         }
     }
   }
